fe8eba192969b48fa2d666f07d9d0f10cee239c1
Ericodesenvolvedor <yuukisouma2003@gmail.com>
2022-07-04 21:45:31 -0300

    fix: Prototipo menu

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,6 +1,22 @@
-const container = document.querySelector('.container');
-const menuBtn = document.querySelector('.nav-menu');
+const btnMenu = document.querySelector('.logo-btn');
+const navigation = document.querySelector('.navigation');
 
-menuBtn.addEventListener('click', () => {
-    container.classList.toggle('active');
+btnMenu.addEventListener('click', () => {
+    navigation.classList.toggle('navOn');
+
+    const navOn = navigation.classList.contains('navOn'); 
+
+    const containerGrid = document.querySelector('.container-grid');        
+    const nav = document.querySelector('.navigation');        
+    const content = document.querySelector('.content-items');   
+
+    if(navOn) {
+        containerGrid.classList.add('row-grid');
+        nav.classList.add('col-grid');
+        content.classList.add('col-grid');
+    } else {
+        containerGrid.classList.remove('row-grid');
+        nav.classList.remove('col-grid');
+        content.classList.remove('col-grid');
+    }
 });

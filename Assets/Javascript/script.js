f2987c17642510a8eb946411123c80b7902a8767
Ericodesenvolvedor <yuukisouma2003@gmail.com>
2022-07-05 21:09:18 -0300

    feat: tamanho de imagem configurada.

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -9,12 +9,17 @@ btnMenu.addEventListener('click', () => {
     const containerGrid = document.querySelector('.container-grid');        
     const nav = document.querySelector('.navigation');        
     const content = document.querySelector('.content-items');   
+    const btnMenuIcon = document.getElementById('btn-menu-icon');
 
     if(navOn) {
         containerGrid.classList.add('row-grid');
         nav.classList.add('col-grid');
         content.classList.add('col-grid');
+        btnMenuIcon.classList.remove('ph-list-bold');
+        btnMenuIcon.classList.add('ph-x-fill');
     } else {
+        btnMenuIcon.classList.add('ph-list-bold');
+        btnMenuIcon.classList.remove('ph-x-fill');
         containerGrid.classList.remove('row-grid');
         nav.classList.remove('col-grid');
         content.classList.remove('col-grid');

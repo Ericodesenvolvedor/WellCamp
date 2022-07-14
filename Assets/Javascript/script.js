61d529460a2cb692a2a6761351bb9d2df25adc52
Jheniffer-fernandes <jheniffer.fernandesm@gmail.com>
2022-07-13 21:53:59 -0300

    feat: atualizando faq

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -12,3 +12,13 @@ faqPlusBtn[count].addEventListener('click', () => {
     let openResponse = document.querySelectorAll('.faq-body');
     openResponse[count].classList.toggle('active-response');
 })
+
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}

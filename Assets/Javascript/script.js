8dacc10503620a81169080b639b00139c6ad4335
Eric <yuukisouma2003@gmail.com>
2022-07-16 20:22:07 -0300

    feat: sessão para receber emails criada

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,5 +1,40 @@
 const hamburguer = document.querySelector('.hamburguer');
+const abrirModal = document.querySelector('#abrir-modal');
+const mostrarModal = document.querySelector('#receber-noticias');
+let inputNome = document.getElementById('nome');
+let inputEmail = document.getElementById('email');
 
 hamburguer.addEventListener('click', () => {
     document.querySelector('.container').classList.toggle('show-menu');
 })
+
+
+
+abrirModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    if(inputNome.value == '') {
+        alert('Você precisa informar todos os campos antes de enviar!');
+    } 
+    
+    else if(inputEmail.value == '') {
+        alert('Você precisa informar todos os campos antes de enviar!');
+    } 
+    
+    else {
+        mostrarModal.classList.add('mostrar-modal');
+        const btnModalFechar = document.querySelector('#fechar-modal');
+        btnModalFechar.addEventListener('click', fecharModal);
+    }
+
+});
+
+function fecharModal() {
+    const mostrarModalOn = mostrarModal.classList.contains('mostrar-modal');
+
+    if(mostrarModalOn) {
+        mostrarModal.classList.remove('mostrar-modal');
+        inputNome.value = '';
+        inputEmail.value = '';
+    }
+}
